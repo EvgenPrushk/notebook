02ace96c22f22f6806909e215052726e6cb00a36
Прушак Евгений Владимирович <D:\poshta>
2020-03-24 21:55:33 +0300

    feat: add project file

---
+++ Database.js
@@ -0,0 +1,18 @@
+;(function () {
+    "use strict"
+
+    const api = {
+
+    };
+
+    api.getNotes = function getNoyes () {
+        return notes;
+    }
+
+    api.setNotes = function setNotes (notes) {
+        
+    }
+    
+    window.database = api;
+
+})();
