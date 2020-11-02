84739cc6d1aa73a5bcf195270079a2aa49a08b13
Прушак Евгений Владимирович <D:\poshta>
2020-11-02 19:01:57 +0300

    feat: add aplication js

---
+++ Application.js
@@ -0,0 +1,16 @@
+
+class Application {
+    constructor () {
+        this.notes = database.getNotes();
+       const buttons = document.querySelector('[data-buttons]');
+
+       this.updateButtons()
+    }
+
+    updateButtons () {
+        // очищает содержимое баттанов
+        this.buttons.innerHTML = '';
+        
+        const buttonElement = 
+    }
+};
--- Database.js
+++ Database.js
@@ -1,18 +1,51 @@
-;(function () {
+// use экранированное пространство имен
+; (function () {
     "use strict"
+
+    const notes = [
+        {
+            id: 1,
+            title: "Список полезных дел",
+            contenet: "Список полезных дел"
+        },    
+        {
+            id: 2,
+            title: "Лучшее за неделю",
+            contenet: "Лучшее за неделю"
+        },     
+        {
+            id: 3,
+            title: "Фильмы на прокат",
+            contenet: `Фильмы на прокат
+            1. Вечно молодой
+            2. Гори гори ясно
+            3. Призрак в доспехах
+            4. Назад в будущее`
+        },        
+        {
+            id: 4,
+            title: "Интенсивы на constcode.ru",
+            contenet: "Интенсивы на constcode.ru"
+        }
+    ];
 
     const api = {
 
     };
 
-    api.getNotes = function getNoyes () {
-        return notes;
+    api.getNotes = function getNoyes() {
+        return getCopy(notes);
     }
 
-    api.setNotes = function setNotes (notes) {
-        
+    api.setNotes = function setNotes(notes) {
+
     }
-    
+
     window.database = api;
+    // защита информации от манипуляций пользователя для этого создаем копию объекта
+    // сюрилизация и ансюрилизация
+    function getCopy(obj) {
+        return JSON.parse(JSON.stringify(obj));
+    }
 
 })();
