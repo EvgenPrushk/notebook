68f9b7b36ca157594b27f88960f380b53f7eff00
EvgenPrushk <ogmprushak@mail.ru>
2021-01-25 16:21:55 +0300

    feat: add metod updatebuttons

--- Application.js
+++ Application.js
@@ -1,8 +1,10 @@
 class Application {
     constructor () {
+        this.noteId = null;
         // когда создается экземпляр класса Application запрашиваю копию массива из database
         this.notes = database.getNotes();
         this.bottons = document.querySelector('[data-buttons]');
+        this.textarea = document.querySelector('textarea');
         
         this.updateButtons();
     }
@@ -10,6 +12,31 @@ class Application {
     updateButtons () {
         this.bottons.innerHTML = '';
 
-        
+        for (const note of this.notes) {
+            const buttonElement = document.createElement('button');
+            buttonElement.className = 'list-group-item list-group-item-action';
+            buttonElement.textContent = note.title;
+            // id необходим для того, чтобы понимать по какой из 
+            // записей произошел клик
+            buttonElement.setAttribute("note-id", note.id);
+
+            if (note.id === this.noteId) {
+                buttonElement.classList.add("active");
+            }
+    
+            this.bottons.append(buttonElement);
+            // стрелочная функция не создает новый контекст, а использует родительский контекст
+            buttonElement.addEventListener('click', () => {
+                this.noteId = parseInt(buttonElement.getAttribute('note-id'));
+                // передает число noteId
+                this.activeNote();
+            })
+        }                      
+    }
+
+    activeNote () {
+        this.updateButtons();
+        const note = this.notes.find(x => x.id === this.noteId);
+        this.textarea.value = note.content; 
     }
 }
